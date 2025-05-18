43cc8e551d107af01075edd00bc749b11fd9fe22
Robert Wiley <ylee@bodhilinux.com>
2025-05-18 11:19:48 -0400

    Improve legacy build script

--- build-legacy.sh
+++ build-legacy.sh
@@ -3,9 +3,10 @@
 
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd MokshaKL4K
-sed -i 's/MokshaKL4K/Icons-Moksha-KLK4/g' angelic.edc
-sed -i 's/MokshaKL4K/MokshaKL4K-GTK/g' angelic.edc
+sed -i \
+    -e 's/item: "gtk-theme" *"[^"]*"/item: "gtk-theme"     "MokshaKL4K-GTK"/' \
+    -e 's/item: "icon-theme" *"[^"]*"/item: "icon-theme"   "Icons-Moksha-KLK4"/' \
+    angelic.edc
 ./build.sh
-sed -i 's/Icons-Moksha-KLK4/MokshaKL4K/g' angelic.edc
-sed -i 's/MokshaKL4K-GTK/MokshaKL4K/g' angelic.edc
+git restore angelic.edc
 cd ..
